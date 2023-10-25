0fd20fcf4669b4f0160f1cbae0a4d4e47ef2c925
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-25 23:01:23 +0530

    Learnt about Flask templates

--- server.py
+++ server.py
@@ -1,8 +1,23 @@
-from flask import Flask
+# Python Flask file to handle the backend
+# Name - Indrajeet Mondal; Date = 25th October 2023
+# SourceCode
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+# A normal routing
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("./index.html")
+
+# A normal routing
+@app.route("/about")
+def about():
+    return render_template("./about.html")
+
+# Creating a blog routing
+@app.route("/blog")
+def hello_bloggers():
+    return "<h1><center>Hello Bloggers!!</center></h1>"
